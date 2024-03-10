d1d2ba193f4985476f845ada904518d4148354a8
Ori Bachar <73943348+OriBachar@users.noreply.github.com>
2024-03-10 13:39:57 +0200

    Add damage to ShooterCharacter

--- Simple Shooter/SimpleShooter/Source/SimpleShooter/Gun.cpp
+++ Simple Shooter/SimpleShooter/Source/SimpleShooter/Gun.cpp
@@ -46,7 +46,16 @@ void AGun::PullTrigger()
 	if(bSuccess) 
 	{
 		FVector ShotDirection = -Rotation.Vector();
+
 		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactEffect, Hit.Location, ShotDirection.Rotation());
+
+		AActor* HitActor =  Hit.GetActor();
+
+		if(HitActor == nullptr) return;
+
+		FPointDamageEvent DamageEvent(Damage, Hit, ShotDirection, nullptr);
+
+		HitActor->TakeDamage(Damage, DamageEvent, OwnerController, this);
 	}
 
 }
