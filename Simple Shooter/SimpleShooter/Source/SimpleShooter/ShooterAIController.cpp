dfd0da45df6c96e7121eb890ddfd5bfb1b46e788
Ori Bachar <73943348+OriBachar@users.noreply.github.com>
2024-03-10 14:55:59 +0200

    Use behavior tree & blackboard

--- Simple Shooter/SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
+++ Simple Shooter/SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -1,13 +1,40 @@
 #include "ShooterAIController.h"
 #include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
 
 
 void AShooterAIController::BeginPlay()
 {
     Super::BeginPlay();
 
+    if (AIBehavior == nullptr) return;
+
+    RunBehaviorTree(AIBehavior);
+
     APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
-    SetFocus(PlayerPawn);
+    GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
 
+    GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+}
+
+void AShooterAIController::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    // APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+    
+    // if(LineOfSightTo(PlayerPawn)) 
+    // {
+    //     SetFocus(PlayerPawn);
+
+    //     MoveToActor(PlayerPawn, AcceptanceRadius);
+    // }
+    // else
+    // {
+    //     ClearFocus(EAIFocusPriority::Gameplay);
+        
+    //     StopMovement();
+    // }
 }
--- Simple Shooter/SimpleShooter/Source/SimpleShooter/ShooterAIController.h
+++ Simple Shooter/SimpleShooter/Source/SimpleShooter/ShooterAIController.h
@@ -17,5 +17,14 @@ class SIMPLESHOOTER_API AShooterAIController : public AAIController
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	class UBehaviorTree* AIBehavior;
+
 	
 };
