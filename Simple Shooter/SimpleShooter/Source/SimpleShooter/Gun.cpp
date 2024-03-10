06dae194814a06032033e74f5666c73a89e3db4f
Ori Bachar <73943348+OriBachar@users.noreply.github.com>
2024-03-10 12:41:23 +0200

    add debug camera

--- Simple Shooter/SimpleShooter/Source/SimpleShooter/Gun.cpp
+++ Simple Shooter/SimpleShooter/Source/SimpleShooter/Gun.cpp
@@ -4,7 +4,7 @@
 #include "Gun.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
-
+#include "DrawDebugHelpers.h"
 // Sets default values
 AGun::AGun()
 {
@@ -22,6 +22,22 @@ AGun::AGun()
 void AGun::PullTrigger()
 {
 	UGameplayStatics::SpawnEmitterAttached(MuzzleFlash, Mesh, TEXT("MuzzleFlashSocket"));
+
+	APawn* OwnerPawn =  Cast<APawn>(GetOwner());
+
+	if(OwnerPawn == nullptr) return;
+
+	AController* OwnerController =  OwnerPawn->GetController();
+
+	if(OwnerController == nullptr) return;
+
+	FVector Location; 
+
+	FRotator Rotation;
+
+	OwnerController->GetPlayerViewPoint(Location, Rotation);
+
+	DrawDebugCamera(GetWorld(), Location, Rotation, 90, 2, FColor::White, true);
 }
 
 // Called when the game starts or when spawned
