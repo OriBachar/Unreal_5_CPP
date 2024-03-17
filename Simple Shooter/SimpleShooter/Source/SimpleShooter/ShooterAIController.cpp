7595422efdeb3781d3a76943d40b20677cad6668
Ori Bachar <73943348+OriBachar@users.noreply.github.com>
2024-03-17 12:49:21 +0200

    Return to start location

--- Simple Shooter/SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
+++ Simple Shooter/SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -1,25 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
 #include "ShooterAIController.h"
+
 #include "Kismet/GameplayStatics.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 
-
-void AShooterAIController::BeginPlay()
+void AShooterAIController::BeginPlay() 
 {
     Super::BeginPlay();
 
-    if (AIBehavior == nullptr) return;
+    if (AIBehavior != nullptr)
+    {
+        RunBehaviorTree(AIBehavior);
 
-    RunBehaviorTree(AIBehavior);
+        APawn *PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
-    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-
-    GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+        GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+    }
 }
 
-void AShooterAIController::Tick(float DeltaTime)
+void AShooterAIController::Tick(float DeltaSeconds)
 {
-    Super::Tick(DeltaTime);
+    Super::Tick(DeltaSeconds);
 
     APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
     
